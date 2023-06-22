7fdce110e49a2ff7e916e9f33270143512f71540
binwen <albertwoo_slaveoftime@hotmail.com>
2023-06-22 18:10:04 +0800

    Fix script

--- build.fsx
+++ build.fsx
@@ -7,45 +7,49 @@ let connections = [5;10;20]
 
 pipeline "bomb" {
     description "start server and test bomb"
-    stage "run server" {
+    stage "build server" {
         workingDir "hello-axum"
         run "cargo build -r"
-        run (fun ctx -> async {
-            ctx.RunCommand("cargo run -r") |> ignore
-            do! Async.Sleep 5000
-            printfn "hello axum server should be up"
-            return Ok()
-        })
     }
-    stage "warm up" {
-        stage "fun-bomb-cs" {
-            workingDir "fun-bomb-cs"
-            run "dotnet run -c Release -- http://127.0.0.1:3000/hello 10"
+    stage "start bomb" {
+        paralle
+        stage "run server" {
+            workingDir "hello-axum"
+            run "cargo run -r"
         }
-        stage "fun-bomb-rs" {
-            workingDir "fun-bomb-rs"
-            run "cargo run -r -q -- http://127.0.0.1:3000/hello 10"
+        stage "run load tools" {
+            run (Async.Sleep 5000)
+            stage "warm up" {
+                stage "fun-bomb-cs" {
+                    workingDir "fun-bomb-cs"
+                    run "dotnet run -c Release -- http://127.0.0.1:3000/hello 10"
+                }
+                stage "fun-bomb-rs" {
+                    workingDir "fun-bomb-rs"
+                    run "cargo run -r -q -- http://127.0.0.1:3000/hello 10"
+                }
+            }
+            stage "fun-bomb-cs" {
+                workingDir "fun-bomb-cs"
+                run (fun ctx -> async {
+                    for connection in connections do
+                        for i in 1..round do
+                            printfn $"Connection {connection}, Round {i} (csharp)"
+                            let! _ = ctx.RunCommand($"dotnet run -c Release -- http://127.0.0.1:3000/hello {connection}")
+                            do! Async.Sleep 10_000
+                })
+            }
+            stage "fun-bomb-rs" {
+                workingDir "fun-bomb-rs"
+                run (fun ctx -> async {
+                    for connection in connections do
+                        for i in 1..round do
+                            printfn $"Connection {connection}, Round {i} (rust)"
+                            let! _ = ctx.RunCommand($"cargo run -r -q -- http://127.0.0.1:3000/hello {connection}")
+                            do! Async.Sleep 10_000
+                })
+            }
         }
-    }
-    stage "fun-bomb-cs" {
-        workingDir "fun-bomb-cs"
-        run (fun ctx -> async {
-            for connection in connections do
-                for i in 1..round do
-                    printfn $"Connection {connection}, Round {i} (csharp)"
-                    let! _ = ctx.RunCommand($"dotnet run -c Release -- http://127.0.0.1:3000/hello {connection}")
-                    do! Async.Sleep 10_000
-        })
-    }
-    stage "fun-bomb-rs" {
-        workingDir "fun-bomb-rs"
-        run (fun ctx -> async {
-            for connection in connections do
-                for i in 1..round do
-                    printfn $"Connection {connection}, Round {i} (rust)"
-                    let! _ = ctx.RunCommand($"cargo run -r -q -- http://127.0.0.1:3000/hello {connection}")
-                    do! Async.Sleep 10_000
-        })
     }
     runIfOnlySpecified false
 }
